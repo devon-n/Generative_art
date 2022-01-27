86e3e7b2f630316fdf9ee2fd0dd0162e48c44252
Devon Nathan <devnathan94@gmail.com>
2022-01-27 16:24:02 +1100

    56.03

--- index.js
+++ index.js
@@ -1,30 +1,107 @@
+const myArgs = process.argv.slice(2)
 const fs = require('fs')
 const { createCanvas, loadImage }  = require('canvas')
-const canvas = createCanvas(1000, 1000)
-const ctx = canvas.getContext('2d')
 const { layers, width, height } = require('./input/config')
+const { resolve } = require('path')
+const canvas = createCanvas(width, height)
+const ctx = canvas.getContext('2d')
+const editionSize = myArgs.length > 0 ? Number(myArgs[0]) : 1
 
-const edition = 1
+var metadata = []
+var attributes = []
+var hash = []
+var decodedHash = []
+var dnaList = []
 
-const saveLayer = (_canvas) => {
-    fs.writeFileSync(`./output/${_edition}.png`, _canvas.toBuffer('image/png'))
+
+
+const saveLayer = (_canvas, _editionSize) => {
+    fs.writeFileSync(`./output/${_editionSize}.png`, _canvas.toBuffer('image/png'))
 }
 
-const drawLayer = async (_layer, _edition) => {
-    let element = _layer.elements[Math.floor(Math.random() * _layer.elements.length)]
-    
-    const image = await loadImage(`${_layer.location}${element.fileName}`)
-    
+const addMetadata = (_editionSize) => {
+    let datetime = Date.now()
+    let tempMetadata = {
+        hash: hash.join(""),
+        decodedHash: decodedHash,
+        editionSize: _editionSize,
+        date: datetime,
+        attributes: attributes
+    }
+    metadata.push(tempMetadata)
+    attributes = []
+    hash = []
+    decodedHash = []
+}
+
+const addAttributes = (_element, _layer) => {
+    let tempAttr = {
+        id: _element.id,
+        layer: _layer.name,
+        name: _element.name,
+        rarity: _element.rarity
+    }
+    attributes.push(tempAttr)
+    hash.push(_layer.id)
+    hash.push(_element.id)
+    decodedHash.push({[_layer.id]: _element.id})
+}
+
+const loadLayer = async (_layer) => {
+    return new Promise(async() => {
+        const image = await loadImage(`${_layer.location}${element.fileName}`)
+        resolve({layer: _layer, loadedImage: image})
+    })
+}
+
+const drawElement = (_element) => {
     ctx.drawImage(image, _layer.position.x, _layer.position.y, _layer.size.width, _layer.size.height)
-    console.log(`Added layer: ${_layer.name} with Element: ${element.name}`)
-    saveLayer(canvas, _edition)
+    addAttributes(_element)
 }
 
-for (let i = 1; i <= edition; i++) {
-    layers.forEach((layer) => {
-        drawLayer(layer, i);
-    });
-    // console.log('Layer: ', i)
+const constructLayerToDna = (_dna, _layer) => {
+    let DnaSegment = _dna.toString().match(/.{1,2}/)
+    console.log(DnaSegment)
 }
 
-drawLayer()
+
+const isDnaUnique = (_dnaList = [1,2], _dna) => {
+    let foundDna = _dnaList.find((i) => i === _dna)
+    return foundDna == undefined ? true : false
+    return true
+}
+
+const createDna = (_len) => {
+    _len = _len * 2 - 1
+    let randNum = Math.floor(Number(`1e${_len}`) + Math.random() * Number(`1e${_len}`))
+    return randNum
+}
+
+const writeMetadata = () => {
+    fs.writeFileSync("./output/_metadata.json", JSON.stringify(metadata))
+}
+
+
+const startCreating = () => {
+    let editionCount = 1;
+    while(editionCount <= editionSize) {
+        let newDna = createDna(layers.length)
+        console.log(newDna)
+
+        if(isDnaUnique(newDna)){
+            let result = constructLayerToDna(newDna, layers)
+
+            // addMetadata(editionCount)
+            console.log('Creating edition ', editionCount)
+            dnaList.push(newDna)
+            editionCount++
+        } else {
+            console.log('Dna exists')
+        }
+        
+    }
+}
+
+
+startCreating()
+writeMetadata()
--- input/config.js
+++ input/config.js
@@ -43,61 +43,48 @@ const getElements = (path) => {
 
 const layers = [
     {
-        id: 1,
-        name: 'background',
         location: `${dir}/background/`,
         elements: getElements(`${dir}/background/`),
         position: {x: 0, y: 0},
         size: {width: width, height: height},
     },
     {
-        id: 2,
-        name: 'ball',
         location: `${dir}/ball/`,
         elements: getElements(`${dir}/ball/`),
         position: {x: 0, y: 0},
         size: {width: width, height: height},
     },
+
     {
-        id: 3,
-        name: 'bottom lid',
-        location: `${dir}/bottom lid/`,
-        elements: getElements(`${dir}/bottom lid/`),
-        position: {x: 0, y: 0},
-        size: {width: width, height: height},
-    },
-    {
-        id: 4,
-        name: 'eye color',
         location: `${dir}/eye color/`,
         elements: getElements(`${dir}/eye color/`),
         position: {x: 0, y: 0},
         size: {width: width, height: height},
     },
     {
-        id: 5,
-        name: 'iris',
         location: `${dir}/iris/`,
         elements: getElements(`${dir}/iris/`),
         position: {x: 0, y: 0},
         size: {width: width, height: height},
     },
     {
-        id: 6,
-        name: 'shine',
         location: `${dir}/shine/`,
         elements: getElements(`${dir}/shine/`),
         position: {x: 0, y: 0},
         size: {width: width, height: height},
     },
     {
-        id: 7,
-        name: 'top lid',
+        location: `${dir}/bottom lid/`,
+        elements: getElements(`${dir}/bottom lid/`),
+        position: {x: 0, y: 0},
+        size: {width: width, height: height},
+    },
+    {
         location: `${dir}/top lid/`,
         elements: getElements(`${dir}/top lid/`),
         position: {x: 0, y: 0},
         size: {width: width, height: height},
-    }
+    },
 ]
 
 module.exports = { layers, width, height }
